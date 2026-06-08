f42110d7482f8cf0d05e934007e5e653f0ffe9bb
seosohard@ya.ru <seosohard@ya.ru>
2026-06-08 22:10:54 +0300

    Добавлено открытие и закрытие попапа через кнопки

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const aboutButton = document.querySelector("#aboutBtn");
+const buttonAbout = document.querySelector("#aboutBtn");
+const buttonClose = document.querySelector("#closePopup");
+const popup = document.querySelector("#aboutPopup");
 
-aboutButton.addEventListener("click", () => {
-  let popup = document.querySelector("#aboutPopup");
+function openPopup() {
   popup.classList.add("active");
-});
+}
+
+function closePopup() {
+  popup.classList.remove("active");
+}
+
+buttonAbout.addEventListener("click", openPopup);
+buttonClose.addEventListener("click", closePopup);
