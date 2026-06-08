d1eea305c6205ac7f135f50111b4efb2c774f036
seosohard@ya.ru <seosohard@ya.ru>
2026-06-08 22:56:55 +0300

    Добавлена блокировка скролла при открытом попапе

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const buttonAbout = document.querySelector("#aboutBtn");
 const buttonClose = document.querySelector("#closePopup");
 const popup = document.querySelector("#aboutPopup");
+const body = document.body;
 
 function openPopup() {
   popup.classList.add("active");
+document.body.classList.add("no-scroll");
 }
 
 function closePopup() {
   popup.classList.remove("active");
+  document.body.classList.remove("no-scroll");
 }
 
 buttonAbout.addEventListener("click", openPopup);
